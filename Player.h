c20123ce7b0698675b5bfe4fc0df357e53172bed
Sky-ma-902 <1723732405@qq.com>
2025-05-24 22:49:45 +0800

    第一次提交

---
+++ Player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QGraphicsItem>
+
+#include<QMediaPlayer>
+#include<QAudioOutput>
+class Player:public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+public:
+    Player(QGraphicsItem * parent=0);
+    void keyPressEvent(QKeyEvent * event);
+public slots:
+    void spawn();
+private:
+    QMediaPlayer * bulletsound;
+    QAudioOutput * audioOutput;
+};
+#endif // PLAYER_H
---
+++ build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Cpp-Plane_FILE D:/Cpp-Plane/Cpp-Plane/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/Cpp-Plane.exe)
+set(__QT_DEPLOY_TARGET_Cpp-Plane_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Cpp-Plane_RUNTIME_DLLS D:/QT/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Multimedia.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Network.dll;D:/QT/6.8.3/mingw_64/bin/Qt6Core.dll)
